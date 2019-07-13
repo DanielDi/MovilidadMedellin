991f1c08c55638d737633d57587e3e59c5d7d5de
Santiago Marín <esmarina@unal.edu.co>
2019-07-13 12:00:28 -0500

    Creación del object Vehiculo.

--- MovilidadMedellin/src/main/Main.scala
+++ MovilidadMedellin/src/main/Main.scala
@@ -2,4 +2,5 @@ package main
 
 object Main extends App {
   println("hola mi perrito x3")
+  var simul = Simulacion
 }
--- MovilidadMedellin/src/main/Simulacion.scala
+++ MovilidadMedellin/src/main/Simulacion.scala
@@ -1,19 +1,30 @@
 package main
-import scala.collection.mutable.ArrayBuffer
+
+import movil.Vehiculo
 import punto.Via
 import movil.Vehiculo
+import scala.collection.mutable.ArrayBuffer
 
 object Simulacion extends Runnable {
   var t = 0
   var dt = 0
   var tRefresh = 0
   var vehiculosMin = 0
-  var vehiculosMax = 0
+  var vehiculosMax = 10
   var velMin = 0
   var velMax = 0
+  var propCarros = 0.2
+  var propMotos = 0.3
+  var propBuses = 0.2
+  var propCamiones = 0.2
+  var propMotoTaxis = 0.1
   
   var arrayDeVehiculos = ArrayBuffer[Vehiculo]()
   var arrayDeVias = ArrayBuffer[Via]()
+  
+  var obj = Vehiculo
+  println(Vehiculo.totalVehiculos)
+  println(arrayDeVehiculos.mkString(", "))
   
   def run() {
     while(true) {
