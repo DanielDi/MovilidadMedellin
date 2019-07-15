9b503f8bb6358cb203bc86caa684cf77e23dbf51
Nicolás <nclscrdn17@gmail.com>
2019-07-15 09:17:41 -0500

    Colocamos las rutas de los archivos de los parametros y resultados

--- MovilidadMedellin/src/main/Main.scala
+++ MovilidadMedellin/src/main/Main.scala
@@ -1,5 +1,16 @@
 package main
 
+import manejoJSON.ManejoJSON
+
 object Main extends App {
-  println("hola mi perrito x3")
+  val ruta = "C:\\Users\\nclsc\\workspace\\MovilidadMedellin\\src\\"
+  val archivoParametros = "parametros.json"
+  val archivoResultados = "resultados.json"
+  
+  var manejoJSON = new ManejoJSON()
+  var parametrosSimulacion = manejoJSON.leerArchivo(ruta + archivoParametros)
+  println(parametrosSimulacion)
+  println(parametrosSimulacion.vehiculos)
+  println(parametrosSimulacion.velocidad)
+  manejoJSON.escribirArchivoResultados(ruta + archivoResultados)
 }
