6b6a15060715c04660722b2540e306650ac50fe7
SebastianLopez <selopezre@unal.edu.co>
2019-07-15 14:09:29 -0500

    Correcciones del grafico y el json

--- MovilidadMedellin/src/main/Main.scala
+++ MovilidadMedellin/src/main/Main.scala
@@ -5,20 +5,14 @@ import movil.Vehiculo
 
 object Main extends App {
   
-  val ruta = "C:\\Users\\nclsc\\workspace\\MovilidadMedellin\\src\\"
+  val ruta = "C:\\Users\\sebas\\git\\MovilidadMedellin\\MovilidadMedellin\\src\\"
   val archivoParametros = "parametros.json"
   val archivoResultados = "resultados.json"
   
   var manejoJSON = new ManejoJSON()
   var parametrosSimulacion = manejoJSON.leerArchivo(ruta + archivoParametros)
-  println(parametrosSimulacion)
-  println(parametrosSimulacion.vehiculos)
-  println(parametrosSimulacion.velocidad)
   manejoJSON.escribirArchivoResultados(ruta + archivoResultados)
 
-  Simulacion.iniciarVias
-  var simul = Simulacion
-  Simulacion.arrayDeVehiculos.foreach(x => println(x.placa))
-
   Simulacion.crearGrafico
+  Vehiculo.crearVehiculos
 }
