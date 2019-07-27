10b8aa0be3b61235c239ce8d64c38b2d9f090ffa
Nicolás <nclscrdn17@gmail.com>
2019-07-27 11:45:46 -0500

    Se hizo el companion object de velocidad y los metodos para convertir

--- MovilidadMedellin/src/punto/Velocidad.scala
+++ MovilidadMedellin/src/punto/Velocidad.scala
@@ -3,3 +3,17 @@ package punto
 class Velocidad(var magnitud: Double, var direccion: Angulo) {
   
 }
+
+object Velocidad {
+  
+  def conversionAKmH(magnitud: Double): Double = {
+    var magnitudKmH = magnitud * 3.6
+    magnitudKmH
+  }
+  
+  def conversionAms(magnitud: Double): Double = {
+    var magnitudMs = magnitud / 3.6
+    magnitudMs
+  }
+  
+}
