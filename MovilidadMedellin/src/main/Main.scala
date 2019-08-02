94fac01a0728736982163993ff5d4b7a83e3495c
Nicolás <nclscrdn17@gmail.com>
2019-08-01 19:16:51 -0500

    ignorar

--- MovilidadMedellin/src/main/Main.scala
+++ MovilidadMedellin/src/main/Main.scala
@@ -4,10 +4,10 @@ import manejoJSON.ManejoJSON
 import movil.Vehiculo
 
 object Main extends App {
-//  val ruta = "C:\\Users\\sebas\\git\\MovilidadMedellin\\MovilidadMedellin\\src\\"
+  val ruta = "C:\\Users\\nclsc\\git\\MovilidadMedellin\\MovilidadMedellin\\src\\"
 //  val ruta = "C:\\Users\\MSI-PC\\git\\MovilidadMedellin\\MovilidadMedellin\\src\\"
 //  val ruta = "C:\\Users\\sebas\\git\\MovilidadMedellin\\MovilidadMedellin\\src\\"
-  val ruta = "C:\\Users\\DELL\\git\\MovilidadMedellin\\MovilidadMedellin\\src\\"
+//  val ruta = "C:\\Users\\DELL\\git\\MovilidadMedellin\\MovilidadMedellin\\src\\"
   val archivoParametros = "parametros.json"
   val archivoResultados = "resultados.json"
   
